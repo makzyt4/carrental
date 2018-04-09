806d9f7c86aed2edc736d55ee62b8f05676d65be
Maksymilian Zytkiewicz <maksymilian.zytkiewicz@gmail.com>
2018-04-10 00:18:13 +0200

    Added logging in

--- src/main/kotlin/pl/makzyt/carrental/controller/IndexController.kt
+++ src/main/kotlin/pl/makzyt/carrental/controller/IndexController.kt
@@ -3,12 +3,23 @@ package pl.makzyt.carrental.controller
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
+import java.security.Principal
 
 @Controller
 class IndexController {
     @GetMapping("/")
+    fun index(principal: Principal?): String {
+        if (principal != null) {
+            println(principal.name)
+        } else {
+            println("none")
+        }
+
+        return "index"
+    }
+
     @PostMapping("/")
-    fun index(): String {
+    fun indexPost(): String {
         return "index"
     }
 }
