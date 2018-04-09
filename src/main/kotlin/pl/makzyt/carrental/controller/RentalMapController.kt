b994355601905e5dcd21056f21f85469c58d3ca3
Maksymilian Zytkiewicz <maksymilian.zytkiewicz@gmail.com>
2018-04-09 14:18:42 +0200

    Added RentalMapController class

--- src/main/kotlin/pl/makzyt/carrental/controller/RentalMapController.kt
+++ src/main/kotlin/pl/makzyt/carrental/controller/RentalMapController.kt
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
-class GreetingController {
-    @GetMapping("/")
-    fun form(): String {
-        return "index"
+class RentalMapController {
+    @GetMapping("/map")
+    fun map(): String {
+        return "map"
     }
 }
