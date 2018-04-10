df4f1e36be44821c9c6ccb9002c00fb91b810f5b
Maksymilian Zytkiewicz <maksymilian.zytkiewicz@gmail.com>
2018-04-10 11:29:44 +0200

    Working log in and log out

--- src/main/kotlin/pl/makzyt/carrental/controller/IndexController.kt
+++ src/main/kotlin/pl/makzyt/carrental/controller/IndexController.kt
@@ -3,12 +3,17 @@ package pl.makzyt.carrental.controller
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
+import java.security.Principal
 
 @Controller
 class IndexController {
     @GetMapping("/")
+    fun index(principal: Principal?): String {
+        return "index"
+    }
+
     @PostMapping("/")
-    fun index(): String {
+    fun indexPost(): String {
         return "index"
     }
 }
